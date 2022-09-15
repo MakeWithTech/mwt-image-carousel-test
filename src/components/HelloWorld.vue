1e1f3ee60a6c788f323959637d2c82459ac36e2a
Irv Shapiro <ishapiro@cogitations.com>
2022-09-15 18:56:02 -0500

    Working API Call Formatting is messed up

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,60 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-carousel v-model="model">
+      <v-carousel-item v-for="myModel in featuredModels" :key="myModel.id">
+        <v-card height="80%">
+            <div class="text-h2">{{ myModel.id }}</div>
+            <p>
+            <v-img :src="myModel.url" height="auto" width="auto"> </v-img>
+            </p>
+        </v-card>
+      </v-carousel-item>
+    </v-carousel>
+  </v-container>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  name: "HelloWorld",
+  data() {
+    return {
+      model: 0,
+      featuredModels: [],
+    };
+  },
+  mounted: async function () {
+    this.makeApiCall();
+  },
+  methods: {
+    say: function (msg) {
+      alert(msg);
+    },
+    makeApiCall: async function () {
+      console.log("About to make call ...");
+      console.log(this.model);
+      console.log(this.featuredModels);
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+      let response;
+      try {
+        response = await axios.get("https://z4mbar3xtwbqlr4gazjr7w7aga0qgnwn.lambda-url.us-east-1.on.aws/v1/models/featured");
+      } catch (error) {
+        console.log("axios get failed: ", error);
+      }
+      if (response) {
+          console.log("Server responded with: ", response);
+          for (let i = 0; i < response.data.items.length; i++) {
+            const myResponse = response.data.items[i];
+            this.featuredModels.push({
+              id: myResponse.id,
+              author: myResponse.author,
+              name: myResponse.name,
+              url: myResponse.url,
+            });
+          }
+          console.log("featured: ", this.featuredModels);
+      }
+    },
+  },
+};
+</script>
