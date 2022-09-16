8d2fcc4e6e65955191e61e0757c6243b8bd962ce
Irv Shapiro <ishapiro@cogitations.com>
2022-09-15 20:35:07 -0500

    Played around with spacing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,30 @@
 <template>
   <v-container>
-    <v-carousel v-model="model">
-      <v-carousel-item v-for="myModel in featuredModels" :key="myModel.id">
-        <v-card height="80%">
-            <div class="text-h2">{{ myModel.id }}</div>
-            <p>
-            <v-img :src="myModel.url" height="auto" width="auto"> </v-img>
-            </p>
-        </v-card>
-      </v-carousel-item>
-    </v-carousel>
+    <v-row justify="space-around">
+      <v-spacer />
+      <v-col col="6">
+        <v-carousel>
+          <v-carousel-item v-for="myModel in featuredModels" :key="myModel.id">
+            <v-card>
+              <v-card-title>{{ myModel.id }}</v-card-title>
+              <v-card-text>
+                <v-img
+                  :src="myModel.url"
+                  aspect="1"
+                  max-height="250"
+                  max-width="500"
+                >
+                </v-img>
+              </v-card-text>
+              <v-card-actions>
+                {{ myModel.author }}
+              </v-card-actions>
+            </v-card>
+          </v-carousel-item>
+        </v-carousel>
+      </v-col>
+      <v-spacer />
+    </v-row>
   </v-container>
 </template>
 
@@ -19,7 +34,6 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      model: 0,
       featuredModels: [],
     };
   },
@@ -37,22 +51,24 @@ export default {
 
       let response;
       try {
-        response = await axios.get("https://z4mbar3xtwbqlr4gazjr7w7aga0qgnwn.lambda-url.us-east-1.on.aws/v1/models/featured");
+        response = await axios.get(
+          "https://z4mbar3xtwbqlr4gazjr7w7aga0qgnwn.lambda-url.us-east-1.on.aws/v1/models/featured"
+        );
       } catch (error) {
         console.log("axios get failed: ", error);
       }
       if (response) {
-          console.log("Server responded with: ", response);
-          for (let i = 0; i < response.data.items.length; i++) {
-            const myResponse = response.data.items[i];
-            this.featuredModels.push({
-              id: myResponse.id,
-              author: myResponse.author,
-              name: myResponse.name,
-              url: myResponse.url,
-            });
-          }
-          console.log("featured: ", this.featuredModels);
+        console.log("Server responded with: ", response);
+        for (let i = 0; i < response.data.items.length; i++) {
+          const myResponse = response.data.items[i];
+          this.featuredModels.push({
+            id: myResponse.id,
+            author: myResponse.author,
+            name: myResponse.name,
+            url: myResponse.url,
+          });
+        }
+        console.log("featured: ", this.featuredModels);
       }
     },
   },
